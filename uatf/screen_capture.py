5a6729e485620f6872216496fe7269ef4ee0fbd5
ar.gayazov <a.gayazov@bk.ru>
2023-07-08 11:55:01 +0500

    element added

--- uatf/screen_capture.py
+++ uatf/screen_capture.py
@@ -5,3 +5,6 @@ from dataclasses import dataclass
 class DrawableType:
     text: str = 'default'
     is_highlighted: bool = True
+
+def make_screen_for_gif(driver, element, type_of_action, data):
+    pass
