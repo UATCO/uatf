b5942d41f3ed878dc44b5ddc60d1bebc82ae8c90
ar.gayazov <a.gayazov@bk.ru>
2023-07-28 15:11:53 +0300

    fix

--- uatf/__init__.py
+++ uatf/__init__.py
@@ -5,3 +5,16 @@ from .pytest_core.base.case import TestCase
 from .logfactory import log, info
 from .ui.region import Region
 from .assert_that import *
+
+
+__all__ = (
+    'log', 'info', 'Config',
+    'TestCase',
+    # assert_that
+    'assert_that', 'instance_of', 'equal_to', 'equal_to_ignoring_case', 'is_not_in', 'and_wait',
+    'not_equal', 'is_not', 'is_', 'is_in', 'is_in_ignoring_case',
+    'less_than', 'less_than_or_equal_to', 'greater_than', 'greater_than_or_equal_to', 'equal_to_xml',
+    'is_in_xml',
+    #base
+    'TestCaseUI'
+)
