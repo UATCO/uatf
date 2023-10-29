b7117a4cc7c5b6159e736af4de558fcb91862b1d
ar.gayazov <>
2023-10-29 14:07:42 +0300

    fix

--- uatf/report/report_base.py
+++ uatf/report/report_base.py
@@ -72,4 +72,4 @@ class ReporBase:
         with open(path, 'w', encoding='utf-8') as file:
             file.write(self.test_logs + '\n\n')
             file.write(self.std_out)
-        return path
+        return path.split('artifact')[1]
