e21767c10c3f095400047cdddce0a9efe021b8ae
ar.gayazov <>
2023-11-18 17:19:44 +0300

    fix

--- uatf/report/templates/report_ui/converter.py
+++ uatf/report/templates/report_ui/converter.py
@@ -1,3 +1,56 @@
+import os
+import codecs
+
+
+class ConvertReport():
+
+    def __init__(self):
+        f = codecs.open("report.html", 'r+', 'utf-8')
+        self.report = f.read()
+
+    def get_product(self):
+        """Получаем название продукта"""
+
+        product = self.report[self.report.index('<h1>') + 4:self.report.index('</h1>')].split(' ')[-1]
+        return product
+
+    def get_test_path(self):
+        """Получаем путь до теста"""
+
+        _ = self.report[self.report.index('<a'): self.report.index('>C')].split('\\')
+        file_name = _[-1].split(':')[0]
+        file_path = f"{self.get_product()}/{_[-4]}/{_[-3]}/{_[-2]}/{file_name}"
+        return os.path.join(os.getenv('ALL_REPORT'), file_path)
+
+    def get_page_path(self):
+        """Получаем путь до теста"""
+
+        _ = self.report[self.report.index('<a'): self.report.index('>C')].split('\\')
+        file_name = _[-1].split(':')[0]
+        file_path = f"{self.get_product()}/{_[-2]}/{file_name}"
+        return os.path.join(os.getenv('ALL_REPORT'), file_path)
+
+    def convert_link_file(self):
+        """Конвертируем ссылку к файлу"""
+
+        for _ in range(self.report.count('/file/C:\\')):
+            old_test_path_with_str_number = self.report[self.report.index('C:'): self.report.index('">C')].split('.py:')
+            if 'test_' in old_test_path_with_str_number[0]:
+                self.report = self.report.replace(f'{old_test_path_with_str_number[0]}.py', self.get_test_path())
+            elif 'pages' in old_test_path_with_str_number[0]:
+                self.report = self.report.replace(f'{old_test_path_with_str_number[0]}.py', self.get_page_path())
+
+        return self.report
+
+    def rewrite_file(self):
+        """Переписываем файл"""
+
+        with open("report.html", 'w') as f:
+            f.write(self.convert_link_file())
+
+
+if __name__ == '__main__':
+    ConvertReport().rewrite_file()
 import glob
 import os
 
